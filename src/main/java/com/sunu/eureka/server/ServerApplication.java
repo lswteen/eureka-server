333673fa25abe820128a11bde30fea3763e08bd5
Unknown <sunu@wemakeprice.com>
2018-09-14 18:39:07 +0900

    master : docker 실행파일만들기 .

--- src/main/java/com/sunu/eureka/server/ServerApplication.java
+++ src/main/java/com/sunu/eureka/server/ServerApplication.java
@@ -1,6 +1,5 @@
 package com.sunu.eureka.server;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
